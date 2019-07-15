f16fe412b8a44f0a90d97ee96e1af1f7cbc84c08
satrong <satrong@163.com>
2019-07-15 23:40:41 +0800

    add files

---
+++ README.md
@@ -0,0 +1,16 @@
+> 将ipa文件重命名，重命名的格式`[ipa包名]--[证书名].ipa`
+
+## 安装
+```shell
+npm i -g ipa-rename
+```
+
+## 使用
+```shell
+ipa-rename [指定文件或文件夹]
+```
+
+如：
+```shell
+ipa-rename ./ipasDirectory
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+// 获取证书名称
+const unzip = require('./lib/unzip');
+const GetPlistInfo = require('./lib/getPlistInfo');
+const getName = require('./lib/getName');
+const fileRename = require('./lib/fileRename');
+const path = require('path');
+const getIpas = require('./lib/getIpas');
+
+// 获取文件名并重命名
+const rename = async filepath => {
+    try {
+        await unzip(filepath);
+        const plist = await GetPlistInfo();
+        fileRename(filepath, getName(plist));
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// 获取ipa文件并重命名
+const getIpasAndRename = async filepath => {
+    const isAbsolute = path.isAbsolute(filepath);
+    if (!isAbsolute) {
+        filepath = path.join(process.cwd(), filepath);
+    }
+    const ipaFilepaths = getIpas(filepath);
+    for (let i = 0, len = ipaFilepaths.length; i < len; i++) {
+        await rename(ipaFilepaths[i]);
+    }
+};
+
+
+if (process.argv.length === 2) {
+    console.log('请指定目录或ipa文件');
+    return process.exit(0);
+}
+
+const filepaths = process.argv.slice(2);
+
+(async () => {
+    for (let i = 0, len = filepaths.length; i < len; i++) {
+        await getIpasAndRename(filepaths[i]);
+    }
+})();
